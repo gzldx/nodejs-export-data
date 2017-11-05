194e3ca8a193eae6ba79c985c18d63682257f737
Xing Lao <xing_lao@me.com>
2017-11-05 20:28:59 +0800

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,79 @@
+module.exports = {
+    leancloud: {
+        //test环境
+        appId: 'fDD8GeyBYL2xpGbcP4S63A0b-gzGzoHsz',
+        appKey: 'CVmRRQ3s3dViVQAs61r8rlWs',
+        masterKey: 'YUMMIxCWq5SpELW8rLHwordN',
+        cacheName: 'mktv_test'
+        //prod环境
+        // appId: 'WB3iKO2JIFtmohE8C4mt0v5t-gzGzoHsz',
+        // appKey: 'AiA91O59uqRxLSQT004siD0e',
+        // masterKey: 'hwQTsAVsEnyUx3STW1oga4cd',
+        // cacheName: 'mktv'
+    },
+    mysql: {
+        db_user: {
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'db_user',
+            port: 3306
+        },
+        db_user_relative: {
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'db_user_relative',
+            port: 3306
+        },
+        db_user_ex: {
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'db_user_ex',
+            port: 3306
+        },
+        db_live: {
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'db_live',
+            port: 3306
+        },
+        db_config: {
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'db_config',
+            port: 3306
+        },
+        db_item: {
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'db_item',
+            port: 3306
+        },
+        db_recharge: {
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'db_recharge',
+            port: 3306
+        },
+        db_trailer: {
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'db_trailer',
+            port: 3306
+        },
+        db_withdraw: {
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'db_withdraw',
+            port: 3306
+        }
+    }
+};
